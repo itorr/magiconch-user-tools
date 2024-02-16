86d041882de5a52d102c902ce6c49db6d54d9a70
itorr-i7 <592668024@qq.com>
2024-02-16 20:09:09 +0800

    调整提示文案

--- tool/js/user-script-installed-test.js
+++ tool/js/user-script-installed-test.js
@@ -5,9 +5,8 @@ document.write(`<article class="not-installed not-installed-box">
 <h2>尚未安装通用小工具功能增强用户脚本</h2>
 <p>未安装功能用户脚本前，页面内功能将无法正常使用。</p>
 <p>
-    点击
-    <a href="/tool/user-script/magiconch-tools-enhance.user.js" target="_blank">magiconch-tools-enhance.user.js</a>
-    进行用户脚本的安装，安装成功后刷新本页面。
+    请返回 <a href="/tool/" target="_blank">首页</a>
+    按照引导安装用户脚本，安装成功后返回本页面即可使用。
 </p>
 </article>`);
 
